eab7f70f698ce905217a749b52242f076195f27f
meet-tw <meet.p@thinkwik.com>
2024-07-03 10:30:47 +0530

    Added code to work node mongo application with docker

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uri = "mongodb://db:27017/products";
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri , 
+        {useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected")
+    }
+    catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    } 
+}
+
+module.exports = connectDB;
